f43a47cea0d3cf41aeddf8ac89979bc8e084cd71
Rijal <rijalghodi.dev@gmail.com>
2025-07-25 23:21:51 +0700

    fix admin firebase

--- src/lib/firebase/firebase-admin.ts
+++ src/lib/firebase/firebase-admin.ts
@@ -1,21 +1,19 @@
 import dotenv from "dotenv";
 import admin from "firebase-admin";
-import { readFileSync } from "fs";
-import path from "path";
 
 import { env } from "../env";
 
 dotenv.config(); // Ensure env is loaded
 
 if (!admin.apps.length) {
-  const serviceAccountPath = process.env.FIREBASE_ADMIN_PATH;
+  // const serviceAccountPath = process.env.FIREBASE_ADMIN_PATH;
 
-  if (!serviceAccountPath) {
-    throw new Error("FIREBASE_ADMIN_PATH not set in .env");
-  }
+  // if (!serviceAccountPath) {
+  //   throw new Error("FIREBASE_ADMIN_PATH not set in .env");
+  // }
 
-  const fullPath = path.resolve(serviceAccountPath);
-  const serviceAccount = JSON.parse(readFileSync(fullPath, "utf8")) as admin.ServiceAccount;
+  // const fullPath = path.resolve(serviceAccountPath);
+  // const serviceAccount = JSON.parse(readFileSync(fullPath, "utf8")) as admin.ServiceAccount;
 
   admin.initializeApp({
     // credential: admin.credential.cert(serviceAccount),
