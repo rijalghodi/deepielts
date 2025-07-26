882a0b1a2ebf51d86c3b5fb7ba376f392fabc8f2
Rijal <rijalghodi.dev@gmail.com>
2025-07-26 07:59:02 +0700

    fix bg gradient

--- src/components/layouts/app-layout.tsx
+++ src/components/layouts/app-layout.tsx
@@ -36,29 +36,29 @@ export function AppLayout({ children }: { children: React.ReactNode }) {
         className={cn("relative w-screen transition-[padding-left] duration-200 ease-linear overflow-hidden")}
         style={{
           paddingLeft: isMobile ? "0" : open && user ? SIDEBAR_WIDTH : user ? SIDEBAR_WIDTH_ICON : "0",
-          marginTop: 60,
+          paddingTop: 60,
         }}
       >
         <FallingStarsBackground className="z-0" />
         <div
           style={{
-            background: "z-0 radial-gradient(at center, rgba(255, 255, 255, 0.02), transparent 50%)",
-            width: 1000,
-            height: 1000,
+            background: "radial-gradient(at center, rgba(255, 255, 255, 0.03), transparent 50%)",
+            width: 1200,
+            height: 1200,
             borderRadius: "100%",
             transform: "translate(-50%, -50%)",
           }}
-          className={`absolute top-0 left-0`}
+          className={`absolute top-0 left-0 z-0`}
         />
         <div
           style={{
-            background: "z-0 radial-gradient(at center, rgba(255, 255, 255, 0.02), transparent 50%)",
-            width: 1000,
-            height: 1000,
+            background: "radial-gradient(at center, rgba(255, 255, 255, 0.03), transparent 50%)",
+            width: 1200,
+            height: 1200,
             borderRadius: "100%",
             transform: "translate(50%, -50%)",
           }}
-          className={`absolute top-0 right-0`}
+          className={`absolute top-0 right-0 z-0`}
         />
         {children}
         <AppFooter />
